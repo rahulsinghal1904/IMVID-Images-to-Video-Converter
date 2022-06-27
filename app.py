6cac638d592a021e416e5afd9e52c9c4f91d7529
Rahul Singhal <rahulsinghal1904@gmail.com>
2022-06-27 10:42:25 +0530

    Download option added

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 
-from flask import Flask, flash, request, redirect, url_for, render_template,redirect
+from flask import Flask, flash, request, redirect, url_for, render_template,redirect,send_file
 import urllib.request
 import os
 import sys
@@ -15,10 +15,60 @@ from wtforms.validators import InputRequired, Length, ValidationError
 from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
+  
+UPLOAD_FOLDER = 'static/uploads/'
+ 
+app = Flask(__name__)
+app.secret_key = "image to videos"
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-app.config['SECRET_KEY'] = 'thisisasecretkey'
+  
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
+    
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+     
+@app.route('/')
+def upload_form():
+    return render_template('index.html')
+ 
+@app.route('/', methods=['POST'])
+def upload_image():
+    if 'files[]' not in request.files:
+        flash('No file part')
+        return redirect(request.url)
+    files = request.files.getlist('files[]')
+    file_names = []
+    video_names = []
+    for file in files:
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file_names.append(UPLOAD_FOLDER+filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+        else:
+            flash('Allowed image types are -> png, jpg, jpeg, gif')
+            return redirect(request.url)
+    frames = [ImageClip(f, duration = 4) for f in file_names]
+    clip = concatenate_videoclips(frames, method = "compose")
+    clip.write_videofile(UPLOAD_FOLDER + 'cvideo.mp4', fps = 24)
+    #images_to_video(file_names, UPLOAD_FOLDER + 'cvideo.mp4')
+    print(file_names, file = sys.stderr)
+    video_names.append('cvideo.mp4')
+    print(video_names, file=sys.stderr)
+    return render_template('index.html', filenames=video_names)
+ 
+@app.route('/display/<filename>')
+def display_video(filename):
+    #print('display_image filename: ' + filename)
+    return redirect(url_for('static', filename='uploads/' + filename), code=301)
 
 
 login_manager = LoginManager()
@@ -64,10 +114,6 @@ class LoginForm(FlaskForm):
     submit = SubmitField('Login')
 
 
-@app.route('/')
-def home():
-    return render_template('home.html')
-
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -107,55 +153,10 @@ def register():
 
     return render_template('register.html', form=form)
 
-app = Flask(__name__)
-  
-UPLOAD_FOLDER = 'static/uploads/'
- 
-app = Flask(__name__)
-app.secret_key = "image to videos"
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
-  
-ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
-
-
-def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-     
-@app.route('/')
-def upload_form():
-    return render_template('index.html')
- 
-@app.route('/', methods=['POST'])
-def upload_image():
-    if 'files[]' not in request.files:
-        flash('No file part')
-        return redirect(request.url)
-    files = request.files.getlist('files[]')
-    file_names = []
-    video_names = []
-    for file in files:
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file_names.append(UPLOAD_FOLDER+filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-
-        else:
-            flash('Allowed image types are -> png, jpg, jpeg, gif')
-            return redirect(request.url)
-    frames = [ImageClip(f, duration = 4) for f in file_names]
-    clip = concatenate_videoclips(frames, method = "compose")
-    clip.write_videofile(UPLOAD_FOLDER + 'cvideo.mp4', fps = 24)
-    #images_to_video(file_names, UPLOAD_FOLDER + 'cvideo.mp4')
-    print(file_names, file = sys.stderr)
-    video_names.append('cvideo.mp4')
-    print(video_names, file=sys.stderr)
-    return render_template('index.html', filenames=video_names)
- 
-@app.route('/display/<filename>')
-def display_video(filename):
-    #print('display_image filename: ' + filename)
-    return redirect(url_for('static', filename='uploads/' + filename), code=301)
+@app.route('/download')
+def download_file():
+    p="./static/uploads/cvideo.mp4"
+    return send_file(p,as_attachment=True)
   
 if __name__ == "__main__":
     app.run(threaded=True)
